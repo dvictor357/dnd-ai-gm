d46e26c7aa29a000fc56f1674d14fdc1ee0ed10d
Dimas Victor <dvictor.1357@outlook.com>
2024-11-20 21:38:05 +0700

    chore: cleaning up old codes

--- app/ai_models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import AIModel
-from .deepseek_model import DeepSeekModel
-from .openrouter_model import OpenRouterModel
-from .factory import AIModelFactory
-
-__all__ = ['AIModel', 'DeepSeekModel', 'OpenRouterModel', 'AIModelFactory']
